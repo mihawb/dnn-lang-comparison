88cab2e86b3bc3373bfd0b02cbf13d082f6baab0
mihawb <michalbanaszczak@gmail.com>
2023-05-03 00:47:31 +0200

    pytorch -> FCNet, resnet18, possibly all models working

--- python/torch_funcs.py
+++ python/torch_funcs.py
@@ -1,4 +1,4 @@
-from load_datasets import load_mnist_imgs_and_labels, load_cifar10_imgs_and_labels
+from load_datasets import load_mnist_imgs_and_labels
 
 import numpy as np
 
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
-from torchvision.transforms import Normalize
+import torchvision
 
 
 def fit(model, device, loader, loss_func, epoch, lr=0.01, optimizer=None, log_interval=100, silent=False):
@@ -19,7 +19,6 @@ def fit(model, device, loader, loss_func, epoch, lr=0.01, optimizer=None, log_in
 
 	for batch_idx, (xb, yb) in enumerate(loader):
 		xb, yb = xb.to(device), yb.to(device)
-		print(xb.size())
 
 		# removing aggregated gradients
 		if optimizer: 
@@ -41,13 +40,14 @@ def fit(model, device, loader, loss_func, epoch, lr=0.01, optimizer=None, log_in
 
 		# telemetry
 		running_loss += loss.item()
-		if batch_idx % log_interval == 0:
+		if batch_idx % log_interval == log_interval - 1:
 			avg_loss = running_loss / log_interval # log_interval == batch count for aggregation
 			running_loss = 0.0
 			logs.append(avg_loss)
+
 			if not silent:
-				print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-					epoch, batch_idx * len(xb), len(loader.dataset),
+				print('Train Epoch: {} -> batch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+					epoch, batch_idx, batch_idx * len(xb), len(loader.dataset),
 					100. * batch_idx / len(loader), avg_loss
 				))
 
@@ -80,8 +80,7 @@ def test(model, device, loader, loss_func, silent=False):
 	return correct_pred / len(loader.dataset)
 
 
-def get_mnist_loaders(batch_size, test_batch_size=None, cutoff=0.8):
-	# do wypierodlenia, od teraz korzystam z datasetow on-site
+def get_mnist_loaders(batch_size, test_batch_size=None, cutoff=1):
 	if not test_batch_size: test_batch_size = batch_size * 2
 
 	x_train, y_train = load_mnist_imgs_and_labels(
@@ -97,17 +96,10 @@ def get_mnist_loaders(batch_size, test_batch_size=None, cutoff=0.8):
 		'../datasets/mnist-digits/t10k-labels.idx1-ubyte'
 	)
 
-	print(type(x_train))
-	print(x_train.dtype)
-
 	x_train, y_train, x_val, y_val, x_test, y_test = map(
 		torch.tensor,
-		# lambda a: torch.tensor(a / 255, dtype=torch.float32),
 		(x_train, y_train, x_val, y_val, x_test, y_test)
 	)
-
-	print(type(x_train))
-	print(x_train.dtype)
 
 	train_ds = TensorDataset(x_train, y_train)
 	val_ds = TensorDataset(x_val, y_val)
@@ -120,35 +112,21 @@ def get_mnist_loaders(batch_size, test_batch_size=None, cutoff=0.8):
 	return train_dl, val_dl, test_dl
 
 
-def get_cifar10_loaders(batch_size, test_batch_size=None, cutoff=0.8):
-	# do wypierdolenia, korzystam teraz z datasetow on-site
+def get_cifar10_loaders(batch_size, test_batch_size=None):
 	if not test_batch_size: test_batch_size = batch_size * 2
 
-	x_train, y_train = load_cifar10_imgs_and_labels('../datasets/cifar-10-py/data_batch_1')
-	for i in range(2,6):
-		x_temp, y_temp = load_cifar10_imgs_and_labels(f'../datasets/cifar-10-py/data_batch_{i}')
-		x_train = np.concatenate((x_train, x_temp))
-		y_train = np.concatenate((y_train, y_temp))
+	transform = torchvision.transforms.Compose([
+		torchvision.transforms.ToTensor(),
+		torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+	])
 
-	x_train, x_val = np.split(x_train, [int(len(x_train) * cutoff)])
-	y_train, y_val = np.split(y_train, [int(len(y_train) * cutoff)])
+	train_ds = torchvision.datasets.CIFAR10(root='../datasets/cifar-10-py/', train=True, download=True, transform=transform)
+	test_ds = torchvision.datasets.CIFAR10(root='../datasets/cifar-10-py/', train=False, download=True, transform=transform)
 
-	x_test, y_test = load_cifar10_imgs_and_labels('../datasets/cifar-10-py/test_batch')
+	train_dl = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=2)
+	test_dl = torch.utils.data.DataLoader(test_ds, batch_size=test_batch_size, shuffle=False, num_workers=2)
 
-	x_train, y_train, x_val, y_val, x_test, y_test = map(
-		torch.tensor,
-		(x_train, y_train, x_val, y_val, x_test, y_test)
-	)
-
-	train_ds = TensorDataset(x_train, y_train)
-	val_ds = TensorDataset(x_val, y_val)
-	test_ds = TensorDataset(x_test, y_test)
-
-	train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-	val_dl = DataLoader(val_ds, batch_size=test_batch_size)
-	test_dl = DataLoader(test_ds, batch_size=test_batch_size)
-
-	return train_dl, val_dl, test_dl
+	return train_dl, test_dl
 
 
 class FullyConnectedNet(nn.Module):
