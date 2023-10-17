7287245c28a2f3f9c99bf63dbf22bae6b7c10704
mihawb <michalbanaszczak@gmail.com>
2023-10-18 01:07:43 +0200

    PT -> TorchScript -> LT model serialization + adjustments and workarounds for torch::jit module

--- libtorch/benchmark_runner.cpp
+++ libtorch/benchmark_runner.cpp
@@ -1,4 +1,5 @@
 #include <torch/torch.h>
+#include <torch/script.h>
 
 #include <nvtx3/nvToolsExt.h>
 #include "driver_types.h"
@@ -6,8 +7,10 @@
 
 #include <iostream>
 #include <fstream>
+#include <memory>
 
 #include "models.h"
+#include "cifar10.h"
 
 int main()
 {
@@ -24,43 +27,73 @@ int main()
     }
 
     std::ofstream results_file;
-    results_file.open("../results/libtorch_fcnet.csv", std::ios::out);
+    results_file.open("../results/libtorch_scvnet_gpu.csv", std::ios::out);
     results_file << "mnames,type,eps,loss,acc,times" << std::endl;
-
-    // new net via reference semantics
-    auto model = std::make_shared<SimpleConvNet>();
-    model->to(device);
 
     // multi-threaded data loader for the MNIST dataset of size [batch_size, 1, 28, 28]
     auto data_loader = torch::data::make_data_loader(
-        torch::data::datasets::MNIST("../datasets/mnist-digits").map(torch::data::transforms::Stack<>()),
+        torch::data::datasets::MNIST("../datasets/mnist-digits", torch::data::datasets::MNIST::Mode::kTrain)
+            .map(torch::data::transforms::Stack<>()),
         /*batch_size=*/32);
 
-    torch::optim::SGD optimizer(model->parameters(), /*lr=*/0.01);
+    // multi-threaded data loader for the CIFAR-10 dataset of size [batch_size, 3, 32, 32]
+    auto data_loader_cifar10 = torch::data::make_data_loader(
+        CIFAR10{"../datasets/cifar-10-binary/cifar-10-batches-bin", CIFAR10::Mode::kTrain}
+            .map(torch::data::transforms::Stack<>()),
+        /*batch_size=*/32);
+
+    // new net via reference semantics
+    // auto model = std::make_shared<FullyConnectedNet>();
+    // auto model = std::make_shared<SimpleConvNet>();
+    torch::jit::script::Module model_to_be_moved = torch::jit::load("./serialized_models/resnet50_for_cifar10.pt");
+    std::shared_ptr<torch::jit::script::Module> model = std::make_shared<torch::jit::script::Module>(model_to_be_moved);
+    if (model == nullptr) {
+        std::cout << "model load error from " << "./serialized_models/resnet50_for_cifar10.pt" << std::endl;
+    }
+
+    model->to(device);
+
+    // workaround, since torch::jit::parameter_list is not supported by torch::optim::SGD
+    std::vector<at::Tensor> model_parameters;
+    for (const auto& params : model->parameters()) {
+        model_parameters.push_back(params);
+    }
+    torch::optim::SGD optimizer(model_parameters, /*lr=*/0.01);
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     float milliseconds = 0;
     float lr = 0.01;
+    model->train();
 
-    for (size_t epoch = 1; epoch <= 15; ++epoch)
+    for (size_t epoch = 1; epoch <= 2; ++epoch)
     {
         size_t batch_index = 0;
         cudaEventRecord(start);
 
-        for (auto &batch : *data_loader)
+        for (auto &batch : *data_loader_cifar10)
         {
             // std::cout << batch.data.sizes() << std::endl;
             // std::cout << batch.target.sizes() << std::endl;
-            
+
             torch::Tensor batch_data = batch.data.to(device);
             torch::Tensor batch_target = batch.target.to(device);
 
             optimizer.zero_grad();
 
-            torch::Tensor prediction = model->forward(batch_data);
-            torch::Tensor loss = torch::nll_loss(prediction, batch_target);
+            // workaround for TorchScript models
+            std::vector<torch::jit::IValue> batch_data_ivalues;
+            batch_data_ivalues.push_back(batch_data);
+            torch::Tensor prediction = model->forward(batch_data_ivalues).toTensor();
+            // for native LibTorch models
+            // torch::Tensor prediction = model->forward(batch_data);
+
+            // for FCNet and SCVNet (last layer is log_softmax)
+            // torch::Tensor loss = torch::nll_loss(prediction, batch_target);
+            //for other models (last layer is softmax or logits? cant remember)
+            torch::Tensor loss = torch::nll_loss(torch::log_softmax(prediction, /*dim=*/1), batch_target);
+
             loss.backward();
             optimizer.step();
 
@@ -78,7 +111,7 @@ int main()
         cudaEventElapsedTime(&milliseconds, start, stop);
 
         std::cout << "Epoch time: " << milliseconds << " ms" << std::endl;
-        results_file << "fcnet"
+        results_file << "scvnet"
                      << ",training," << epoch << "," << -1 << "," << -1 << "," << milliseconds << std::endl;
     }
 
--- libtorch/run_build.sh
+++ libtorch/run_build.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+if ! [ $(find . -name "*.pt" | wc -l) -gt 0 ]; then
+	python model_serializer.py
+fi
+
 if ! [ -d build ]; then
 	mkdir build
 fi
 cd build
+
 cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch; print(torch.utils.cmake_prefix_path)'` ..
+# cmake -DCMAKE_PREFIX_PATH=/home/mihawb/dnn-lang-comparison/libtorch/libtorch-cxx11/share/cmake ..
+# cmake -DCMAKE_PREFIX_PATH=/home/mihawb/dnn-lang-comparison/libtorch/libtorch-pre-cxx11/share/cmake ..
 cmake --build . --config Release
